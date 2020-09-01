faf3da5d3fc236bc32ec4dce2d78f07f34b6c6db
Jose Rebelo <>
2020-09-01 13:49:41 +0100

    enebale cors

--- routes/index.js
+++ routes/index.js
@@ -11,10 +11,10 @@ spellCorrector.loadDictionary();
 const { SentimentAnalyzer } = require("node-nlp");
 const sentiment = new SentimentAnalyzer({ language: "en" });
 
-// const cors = require("cors");
+const cors = require("cors");
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
