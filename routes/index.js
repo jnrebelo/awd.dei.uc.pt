8e9f39adf580949729405aae54de8cad02b48d64
Jose Rebelo <>
2020-09-03 20:13:33 +0100

    test middleware

--- routes/index.js
+++ routes/index.js
@@ -11,11 +11,11 @@ spellCorrector.loadDictionary();
 const { SentimentAnalyzer } = require("node-nlp");
 const sentiment = new SentimentAnalyzer({ language: "en" });
 
-// const cors = require("cors");
+const cors = require("cors");
 const app = express();
 
-// app.use(cors());
-// app.options("*", cors());
+app.use(cors());
+app.options("*", cors());
 
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true, limit: "100mb" }));
